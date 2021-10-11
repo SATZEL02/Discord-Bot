36b8084765a7feb9d647b9d87b7cbccefa98d726
Payal jain <201210056@nitdelhi.ac.in>
2021-10-12 00:49:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print('ok')
-print('I love you Dev')
+import discord
+#import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Hi , I am {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("**Test"):
+        await message.channel.send("Bot is working!!")
+client.run('ODk2ODEyNjQ3NDEwOTc0NzYw.YWMj3Q.WQj39Xxqh5WJs1jgDkmfG9XbfsQ')
