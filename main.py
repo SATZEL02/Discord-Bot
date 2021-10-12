06166ff7bc7a2ca275e808393dddcaa52f542088
SATZEL02 <gargdev89@gmail.com>
2021-10-12 14:49:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ import discord
 #import os
 import json
 import requests
+import random
+
 client = discord.Client()
+
+
+sad = ['sad', 'depression', 'depressed', 'misery', 'miserable', 'dukh', 'angry', 'anger', 'dukhi', 'gussa', 'gnda', 'dard', 'pain', 'unhappy']
+sad_reply = ["Whenever you need to call, I'm here.",
+"I wish I could be there right now.",
+"You're still in my thoughts. Remember that.",
+"Hey, I haven’t forgotten about you or how difficult this must be. You’re showing a lot of strength.",
+"Shit happens to everyone. Not everyone deals with it as well as you."]
 
 @client.event
 async def on_ready():
@@ -22,4 +32,8 @@ async def on_message(message):
     if message.content.startswith("!quote"):
         quote = qoute()
         await message.channel.send(quote)
+    if any(word in message.content for word in sad):
+        await message.channel.send(random.choice(sad_reply))
+
+
 client.run('ODk2ODEyNjQ3NDEwOTc0NzYw.YWMj3Q.WQj39Xxqh5WJs1jgDkmfG9XbfsQ')
